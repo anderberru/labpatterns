f05abd7695f3289b74fce0e516b7f95a29ba347d
Ander Berruezo Aguirre <berru0429@gmail.com>
2023-11-09 11:38:26 +0100

    observer prototipo 3 + uml

--- src/observer/Main.java
+++ src/observer/Main.java
@@ -16,16 +16,19 @@ public class Main {
 		new	PacientObserverGUI(pacient);
 		new	PacientSymptomGUI((Covid19Pacient) pacient);
 		new PacientThermometerGUI(pacient);
+		new SemaphorGUI(pacient);
 		
 		Observable	pacient2=new Covid19Pacient("mikel", 23, sf);
 		new	PacientObserverGUI(pacient2);
 		new	PacientSymptomGUI((Covid19Pacient) pacient2);
 		new PacientThermometerGUI(pacient2);
+		new SemaphorGUI(pacient2);
 		
 		Observable	pacient3=new Covid19Pacient("ane", 19, sf);
 		new	PacientObserverGUI(pacient3);
 		new	PacientSymptomGUI((Covid19Pacient) pacient3);
 		new PacientThermometerGUI(pacient3);
+		new SemaphorGUI(pacient3);
 	}
 
 
