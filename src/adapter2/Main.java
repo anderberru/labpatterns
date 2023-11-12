50f70faffe4c9f57c00f6dfb85abfdbd5e07554a
Damian Vela Palacios <damianvelapal@gmail.com>
2023-11-12 19:01:59 +0100

    adapter

--- src/adapter2/Main.java
+++ src/adapter2/Main.java
@@ -13,10 +13,20 @@ public class Main {
 		pacient.addSymptomByName("cefalea", 1);
 		pacient.addSymptomByName("astenia", 3);
 		
+		Covid19Pacient pacient2=new Covid19Pacient("juan", 35, sf);
+		
+		pacient2.addSymptomByName("disnea", 2);
+		pacient2.addSymptomByName("cefalea", 1);
+		pacient2.addSymptomByName("astenia", 3);
+		
 		ShowPacientTableGUI gui=new ShowPacientTableGUI(pacient);
+		ShowPacientTableGUI gui2=new ShowPacientTableGUI(pacient2);
 		gui.setPreferredSize(
 			      new java.awt.Dimension(300, 200));
 		gui.setVisible(true);
+		gui2.setPreferredSize(
+			      new java.awt.Dimension(300, 200));
+		gui2.setVisible(true);
 
 
 	}
